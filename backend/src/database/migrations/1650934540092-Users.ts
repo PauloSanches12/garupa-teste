8daa9c4b26e0ca6443175f40f753f0dd8b26497b
Paulo <paulo_sanches2012@hotmail.com>
2022-04-25 22:31:32 -0300

    criando migration de users

---
+++ backend/src/database/migrations/1650934540092-Users.ts
@@ -0,0 +1,50 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class Users1650934540092 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "users",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true
+                    },
+                    {
+                        name: "name",
+                        type: "varchar"
+                    },
+                    {
+                        name: "phone",
+                        type: "int"
+                    },
+                    {
+                        name: "email",
+                        type: "varchar"
+                    },
+                    {
+                        name: "city",
+                        type: "varchar"
+                    },
+                    {
+                        name: "state",
+                        type: "char",
+                        length: "2"
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()"
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("users");
+    }
+
+}
