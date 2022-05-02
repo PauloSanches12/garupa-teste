a9a2ae894255036569cb50f269420e2affa2771f
Paulo <paulo_sanches2012@hotmail.com>
2022-05-01 21:17:05 -0300

    alterando nomenclatura do middleware

--- backend/src/middleware/userAuthentication.ts
+++ backend/src/middleware/userAuthentication.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 
-export const auth = async (request: Request, response: Response, next: NextFunction) => {    
+export const userAuthentication = async (request: Request, response: Response, next: NextFunction) => {    
     const token = request.header("_token");
 
     if (!token || token !== process.env.TOKEN_SECRET){
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import { UserController } from "./controllers/UserController";
-import { auth } from "./middleware/userAuthentication";
+import { userAuthentication } from "./middleware/userAuthentication";
 
 const routes = Router();
 
 const { createUser, getAllUsers, deleteUser, updateUser } = new UserController();
 
-routes.post("/users", auth, createUser);
-routes.get("/users", auth, getAllUsers);
-routes.delete("/users/:id", auth, deleteUser);
-routes.put("/users/:id", auth, updateUser);
+routes.post("/users", userAuthentication, createUser);
+routes.get("/users", userAuthentication, getAllUsers);
+routes.delete("/users/:id", userAuthentication, deleteUser);
+routes.put("/users/:id", userAuthentication, updateUser);
 
 export { routes };
