9b8e99a48fc0e525be8558af45a2bfe0b99d44e3
Paulo <paulo_sanches2012@hotmail.com>
2022-05-01 23:26:59 -0300

    adicionando cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,9 +2,11 @@ import "reflect-metadata";
 import express, { json } from "express";
 import "./database";
 import { routes } from "./routes";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
 app.use(json());
 
 app.use(routes);
